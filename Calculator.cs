608f8a30a5df4e62d07804eb2915e210d6e408a6
raflipujiadi <pujiadirafli@gmail.com>
2020-03-13 22:28:40 +0700

    Melengkapi fungsi Main

--- Calculator.cs
+++ Calculator.cs
@@ -19,6 +19,9 @@ namespace CalculatorConsoleApp
     {
         static void Main(string[] args)
         {
+            int a = 10;
+            int b = 6;
+
             Console.WriteLine("Hasil pengurangan {0} + {1} = {2}", a, b, Penambahan(a, b));
             Console.WriteLine("Hasil pengurangan {0} - {1} = {2}", a, b, Pengurangan(a, b));
 
