884c43548b2ed648eca08374bda3c565834c7cdc
raflipujiadi <pujiadirafli@gmail.com>
2020-03-13 22:18:44 +0700

    Membuat struktur kode aplikasi calculator

--- Calculator.cs
+++ Calculator.cs
@@ -1,3 +1,11 @@
+/******************************************************************************
+
+                            Online C# Compiler.
+                Code, Compile, Run and Debug C# program online.
+Write your code in this editor and press "Run" button to execute it.
+
+*******************************************************************************/
+
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -7,43 +15,12 @@ using System.Threading.Tasks;
 
 namespace CalculatorConsoleApp
 {
-    class Program
+    class Calculator
     {
         static void Main(string[] args)
         {
-            Console.Title = "Aplikasi Kalkulator";
-
-            Console.Write("Inputkan nilai a =");
-            int a = int.Parse(Console.ReadLine());
-
-            Console.Write("Inputkan nilai b =");
-            int b = Convert.ToInt32(Console.ReadLine());
-
-            Console.WriteLine();
-
-            Console.WriteLine("Hasil penambahan " + a + "+" + b + " = " + Penambahan(a, b));
-            Console.WriteLine("Hasil pengurangan {0} - {1} = {2}", a, b, Pengurangan(a, b));
-            Console.WriteLine("Hasil perkalian {0} * {1} = {2}", a, b, Perkalian(a, b));
-            Console.WriteLine("Hasil pembagian {0} / {1} = {2}", a, b, Pembagian(a, b));
-
-            Console.WriteLine("Tekan sembarang untuk keluar");
-            Console.ReadKey();
+            
         }
-        static int Penambahan(int a, int b)
-        {
-            return a + b;
-        }
-        static int Pengurangan(int a, int b)
-        {
-            return a - b;
-        }
-        static int Perkalian(int a, int b)
-        {
-            return a * b;
-        }
-        static int Pembagian(int a, int b)
-        {
-            return a / b;
-        }
+        
     }
 }
